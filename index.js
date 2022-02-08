d653ccef28053fa7037ca3d9542ebfca2c9c895e
Hasi-b <hasib.hasan9@gmail.com>
2022-02-08 22:05:00 +0000

    bot fetching random inspirational quotes

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const Discord = require("discord.js");
+const fetch = require("node-fetch-commonjs")
 
 const mySecret = process.env['TOKEN'];
 const client = new Discord.Client({
   intents: ["GUILDS","GUILD_MESSAGES"]
   })
 
-  client.on("ready", ()=>{
-    console.log(`Logged in as ${client.user.tag}`)
+function getJokes(){
+  return fetch("https://zenquotes.io/api/random").then(res=>{
+    return res.json()
+  }).then(data =>{
+    return data[0]["q"] + "-" + data[0]["a"]
   })
-  client.login(mySecret);
+}
+
+
+
+
+
+
+
+
+client.on("ready", ()=>{
+  console.log(`Logged in as ${client.user.tag}`)
+})
+  
+
+client.on("message",msg=>{
+if(msg.author.bot) return
+
+if(msg.content === "$shoot"){
+  getJokes().then(quote=> msg.channel.send(quote))
+}
+
+
+  if(msg.content === "ping"){
+     msg.reply("pong")
+  }
+   
+
+})
+client.login(mySecret);
 
